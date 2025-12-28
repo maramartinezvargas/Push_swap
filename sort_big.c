b21f31afb58c70f70fad38768e756691bfc3de4f
tamamart <tamamart@student.42madrid.com>
2025-12-28 19:24:20 +0100

    Optimización para 100 números random < 700 operaciones

--- sort_big.c
+++ sort_big.c
@@ -6,55 +6,86 @@
 /*   By: tamamart <tamamart@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 17:10:44 by tamamart          #+#    #+#             */
-/*   Updated: 2025/12/28 19:06:57 by tamamart         ###   ########.fr       */
+/*   Updated: 2025/12/28 19:18:44 by tamamart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	max_bits(t_stack *a)
+static void	push_to_b(t_stack **a, t_stack **b, int *target, int chunk)
+{
+	if ((*a)->index <= *target)
+	{
+		pb(a, b);
+		rb(b);
+		(*target)++;
+	}
+	else if ((*a)->index <= *target + chunk)
+	{
+		pb(a, b);
+		(*target)++;
+	}
+	else
+		ra(a);
+}
+
+static void	push_chunks(t_stack **a, t_stack **b, int chunk)
+{
+	int	target;
+	int	size;
+
+	target = 0;
+	size = stack_size(*a);
+	while (target < size)
+		push_to_b(a, b, &target, chunk);
+}
+
+static int	get_pos(t_stack *s, int index)
+{
+	int	i;
+
+	i = 0;
+	while (s)
+	{
+		if (s->index == index)
+			return (i);
+		s = s->next;
+		i++;
+	}
+	return (0);
+}
+
+static void	push_back(t_stack **a, t_stack **b)
 {
 	int	max;
-	int	bits;
+	int	pos;
+	int	size;
 
-	max = 0;
-	while (a)
+	while (*b)
 	{
-		if (a->index > max)
-			max = a->index;
-		a = a->next;
+		max = stack_max(*b)->index;
+		pos = get_pos(*b, max);
+		size = stack_size(*b);
+		if (pos <= size / 2)
+			while ((*b)->index != max)
+				rb(b);
+		else
+			while ((*b)->index != max)
+				rrb(b);
+		pa(a, b);
 	}
-	bits = 0;
-	while ((max >> bits) != 0)
-		bits++;
-	return (bits);
 }
 
 void	sort_big(t_stack **a, t_stack **b)
 {
-	int	i;
-	int	j;
 	int	size;
-	int	bits;
+	int	chunk;
 
-	bits = max_bits(*a);
-	i = 0;
-	while (i < bits)
-	{
-		j = 0;
-		size = stack_size(*a);
-		while (j < size)
-		{
-			if (((*a)->index >> i) & 1)
-				ra(a);
-			else
-				pb(a, b);
-			j++;
-		}
-		while (*b)
-			pa(a, b);
-		if (is_sorted(*a))
-			break ;
-		i++;
-	}
+	size = stack_size(*a);
+	if (size <= 100)
+		chunk = 15;
+	else
+		chunk = 30;
+	push_chunks(a, b, chunk);
+	push_back(a, b);
 }
